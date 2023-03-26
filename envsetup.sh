476d3e77f9ab2c39cb282f28c134d54a5f099204
wuliyong <wuliyong3155@163.com>
2023-03-26 16:09:53 +0800

    urbetter-linux: update lcd proj config file
    update lcd proj config file
    update envsetup for build example
    
    Change-Id: I9a59de9685073b67276c6a30e65cb724e8dfe3bf
    Signed-off-by: wuliyong <wuliyong3155@163.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1,2 +1,8 @@
 export KBUILD_OUTPUT=out_kernel2.6.28
+#make xxx_defconfig
+#make -j24 2>&1 | tee build.log
+#git add .
+#git commit -s
+#git commit -s --amend
+#git push origin HEAD:refs/for/master -o wip
 
